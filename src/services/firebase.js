271043e6343cfb06be2158492eaa7509cfbdf269
Vinicius Rivaldar dos Santos Silva <149613214+ViniRivaldar@users.noreply.github.com>
2024-08-20 14:51:39 -0300

    Update firebase.js

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -34,7 +34,7 @@ export const uploadImage = (req,res,next)=>{
 
       await file.makePublic()
 
-      req.file.firebaseUrl = `https://firebasestorage.googleapis.com/v0/b/${BUCKET}/${nomeDoArquivo}`
+      req.file.firebaseUrl = `https://firebasestorage.googleapis.com/v0/b/${BUCKET}/o/${encodeURIComponent(nomeDoArquivo)}?alt=media`
 
       next()
   })
